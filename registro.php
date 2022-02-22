fef1fd04b9aea74fc5ffee64844840ca4d429399
JoseCarlosVC <9705@iespuertasdelcampo.es>
2022-02-22 09:31:44 +0100

    m

--- registro.php
+++ registro.php
@@ -1,46 +1,45 @@
 <?php
-    ini_set('display_errors', 1);
-    error_reporting(E_ALL);
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
 
-    if(isset($_POST['submit'])){
-        //Comprobamos que la imagen se ha enviado
-        $comprobar = getimagesize(($_FILES['imagen']['tmp_name']));    
-        if($comprobar !== false){
-            //Comprobamos que todos los campos necesarios han sido rellenados en el formulario
-            if(isset($_POST['nombre']) && isset($_POST['correo']) && isset($_POST['apellido1']) && isset($_POST['fechaNac']) && isset($_POST['passwd'])){
-                $nombre = $_POST['nombre'];
-                $correo = $_POST['correo'];
-                $apellido1 = $_POST['apellido1'];
-                $fechaNac = $_POST['fechaNac'];
-                //Encriptación de la contraseña
-                $passwd = password_hash($_POST['passwd'],PASSWORD_DEFAULT);
-                //Almacenamos la imagen y la preparamos para su inserción en base de datos
-                $imagen = $_FILES['imagen']['tmp_name'];
-                $fotoPerfil = addslashes(file_get_contents($imagen));
-                //Formateamos la fecha para almacenarla en la base de datos
-                $fechaNac = date('Y-m-d', strtotime($_POST['fechaNac']));
-                //El segundo apellido es opcional, si se ha introducido lo recogemos, si no, lo dejamos vacío
-                if(isset($_POST['apellido2'])){
-                    $apellido2 = $_POST['apellido2'];
-                }else{
-                    $apellido2 = "";
-                }
-                require_once("./app/conexion.inc.php");
-                $conexion = Conexion::openConexion();
-                //Primero comprobamos si el usuario existe en la base de datos
-                $existe = $conexion->query("SELECT * FROM Usuario WHERE correo='$correo'");
-                if($registro = $existe->fetch()){
-                    echo "Este correo ya existe en la base de datos";
-                    //TODO redireccionar al registro
-                }else{
-                    try{
-                        $insertar = $conexion->exec("INSERT INTO Usuario (nombre,correo,pass,foto,apellido1,apellido2,fechaNac) VALUES ('$nombre','$correo','$passwd','$fotoPerfil','$apellido1','$apellido2','$fechaNac')");
-                        echo "REgistrto completo";
-                    }catch(PDOException $err){
-                        echo "Error insertando el usuario: ". $err->getMessage();
-                    }
+if (isset($_POST['submit'])) {
+    //Comprobamos que la imagen se ha enviado
+    $comprobar = getimagesize(($_FILES['imagen']['tmp_name']));
+    if ($comprobar !== false) {
+        //Comprobamos que todos los campos necesarios han sido rellenados en el formulario
+        if (isset($_POST['nombre']) && isset($_POST['correo']) && isset($_POST['apellido1']) && isset($_POST['fechaNac']) && isset($_POST['passwd'])) {
+            $nombre = $_POST['nombre'];
+            $correo = $_POST['correo'];
+            $apellido1 = $_POST['apellido1'];
+            $fechaNac = $_POST['fechaNac'];
+            //Encriptación de la contraseña
+            $passwd = password_hash($_POST['passwd'], PASSWORD_DEFAULT);
+            //Almacenamos la imagen y la preparamos para su inserción en base de datos
+            $imagen = $_FILES['imagen']['tmp_name'];
+            $fotoPerfil = addslashes(file_get_contents($imagen));
+            //Formateamos la fecha para almacenarla en la base de datos
+            $fechaNac = date('Y-m-d', strtotime($_POST['fechaNac']));
+            //El segundo apellido es opcional, si se ha introducido lo recogemos, si no, lo dejamos vacío
+            if (isset($_POST['apellido2'])) {
+                $apellido2 = $_POST['apellido2'];
+            } else {
+                $apellido2 = "";
+            }
+            require_once("./app/conexion.inc.php");
+            $conexion = Conexion::openConexion();
+            //Primero comprobamos si el usuario existe en la base de datos
+            $existe = $conexion->query("SELECT * FROM Usuario WHERE correo='$correo'");
+            if ($registro = $existe->fetch()) {
+                echo "Este correo ya existe en la base de datos";
+                //TODO redireccionar al registro
+            } else {
+                try {
+                    $insertar = $conexion->exec("INSERT INTO Usuario (nombre,correo,pass,foto,apellido1,apellido2,fechaNac) VALUES ('$nombre','$correo','$passwd','$fotoPerfil','$apellido1','$apellido2','$fechaNac')");
+                    echo "REgistrto completo";
+                } catch (PDOException $err) {
+                    echo "Error insertando el usuario: " . $err->getMessage();
                 }
             }
         }
     }
-
+}
